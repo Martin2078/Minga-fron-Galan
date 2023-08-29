8d6771f46ffd4c8d67829b976521141430e2c465
Martin <martinleonelgalan@gmail.com>
2023-08-29 15:42:22 -0300

    app

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,10 +1,10 @@
 import flecha from '../../public/images/arrow-right.svg'
 import React from 'react'
 
-const Arrow = ({props}) => {
+const Arrow = ({props,func}) => {
   return (
-    <button className='border-solid bg-[#999] rounded-full p-2'>
-      <img className={`${props} text-[#333333] w-6 `} src={flecha} alt="" />
+    <button className='border-solid bg-[#999] rounded-full p-2' onClick={func}>
+      <img className={`${props} text-[#333333] w-3 `} src={flecha} alt="" />
       </button>
   )
 }
