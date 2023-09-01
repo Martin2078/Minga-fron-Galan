c352d7d52000b82527e88855f4a74cfcd92a68c5
NazaRamos <nazareno.94@hotmail.com>
2023-09-01 18:44:57 -0300

    Ajuste carrusel y cambio de API

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -8,7 +8,7 @@ const Carrousel = () => {
 
 
   useEffect(()=>{
-    axios("https://minga-back-vyqy.onrender.com/categories")
+    axios("localhost:4000/categories")
      .then(res=> setCategories(res.data.categories))
      .catch(error=>console.log(error))  
 }, [])
@@ -23,8 +23,8 @@ let prev =()=> (count!==0) ?  setCount(count-1) : setCount(categories.length-1);
     <div className='flex bg-[#4338CA] w-11/12 h-4/6 rounded justify-between items-center px-10'>
             <Arrow props="rotate-180" func={prev}/>
             <div className='flex gap-x-24 items-center h-full'>
-              <img className='h-52 absolute left-36 bottom-14' src={categories[count]?.character_photo } alt="" />
-              <img className='h-48 absolute left-1/3 bottom-14 ' src={categories[count]?.cover_photo} alt="" />
+              <img className='h-44 absolute left-36 bottom-10' src={categories[count]?.character_photo } alt="" />
+              <img className='h-40 absolute left-1/3 bottom-12' src={categories[count]?.cover_photo} alt="" />
 
             <div className='w-1/4 text-justify absolute right-60'>
               <h2 className='text-2xl text-[#FFFFFF] font-sans["Roboto"]'>{categories[count]?.name}</h2>
