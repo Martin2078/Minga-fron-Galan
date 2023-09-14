c8c275ed24a1640079b3387b304a41812001ab3b
juan corral <juanaysencorral@gmail.com>
2023-09-14 07:08:38 -0300

    final

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,11 +2,14 @@ import React, { useDebugValue, useState }  from "react";
 import axios from "axios";
 import RegisterForm from "../components/RegisterForm";
 import { useNavigate } from "react-router-dom";
+import Alert from "../components/Alert";
 
 const SignUp =()=>{
-const [showAlert, setShowAlert] = useState(false);
-const [error,setError] = useState([]);
-let errorData =[]
+const [dataForAlert, setDataForAlert] = useState([])
+const [show, setShow] = useState(false)
+
+const [response, setResponse ] = useState(false)
+let dataMessage =[]
 let navigate = useNavigate()
     const handleCreate = (e,data)=>{
         
@@ -14,43 +17,56 @@ let navigate = useNavigate()
         create(data);
           }
      
-          
-          
+          const setShowAlert = ()=>{
+            if(show){
+                if(response){
+                    setShow(false)
+                    navigate("/");
+                }else{
+                    setShow(false)
+                }
+            } else{
+                setShow(true)
+            }
+            
+          }
+        
 
         const create = async (data) =>{
            await axios.post("http://localhost:4000/users/signup",data)
             .then(res =>{ 
                 console.log(res.data.message)
                 if(res.data.success){
-                    logIn(data)
+                    setResponse(true)
+                    dataMessage.push(res.data.message)
+                    setDataForAlert(dataMessage)
                 }else{
-                    errorData.push(res.data.message)
-                    setError(errorData)
+                    dataMessage.push(res.data.message)
+                    setDataForAlert(errorData)
                 }
                 
+                setShow(true)
                 }
                 )
             .catch(error =>{
-                 setError(error.response.data.message)
+                console.log(error)
+                if(error.response){
+                  setDataForAlert(error.response.data.message)
+                  setShow(true)
+                } else{
+                    dataMessage.push("Error del servidor")
+                    setDataForAlert(dataMessage)
+                    setShow(true)
+                }
+                
+                
                 })
         }
-        console.log(error)
         
-        const logIn = async (data) =>{
-            await axios.post("http://localhost:4000/users/signin",{"email": data.email,"password":data.password})
-            .then(res=>{
-                console.log(res)
-                redirect()
-            })
-            .catch(error=>{
-
-                console.log(error)
-            })
-        }
-
-        const redirect = () =>{
-            navigate("/")
-        }
+        
+        
+        
+        
     
 
 
@@ -58,7 +74,7 @@ let navigate = useNavigate()
 
         <div className="h-screen w-full flex flex-row-reverse  ">
             <div className="hidden sm:flex sm:w-1/2 sm:h-full "><img className="w-full h-full object-cover" src="../images/Rectangle 80.png" alt="" /></div>
-            <div className="w-full h-1/2 flex flex-col items-center  gap-1 sm:w-1/2">
+            <div className="w-full h-1/2 flex flex-col items-center  gap-1 sm:w-1/2 pt-10">
             <div className='flex h-10'>
               <img className='w-20' src="../images/logo-footer.png" alt="" />
               <img className='w-20' src="../images/logo-footer2.png" alt="" />
@@ -67,11 +83,20 @@ let navigate = useNavigate()
             <h1 className="text-xl">welcome</h1>
             <p className="w-2/3 text-xs">Discover manga, manhua and manhwa, track your progress, have fun, read manga.</p>
            </section> 
+           
           <RegisterForm
           handleCreate = {handleCreate}
           
           />
            </div>
+           {show ? <Alert
+           show ={show}
+           setShow = {setShowAlert}
+           message={dataForAlert}
+
+           /> :
+           null }
+           
             
         </div>
     )
