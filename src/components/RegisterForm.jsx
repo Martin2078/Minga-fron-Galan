c58856987b1d95fb048ab8c158efdcfd39393ec9
Martin <martinleonelgalan@gmail.com>
2023-09-29 15:38:23 -0300

    app

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useRef } from 'react';
-import {isMobile} from 'react-device-detect'
-
 
 const RegisterForm =(props) =>{
     
@@ -15,6 +13,7 @@ const RegisterForm =(props) =>{
     })
 
    const email=useRef()
+   const photo=useRef()
    const password=useRef()
 
     function setPhoto(e) {
@@ -31,7 +30,12 @@ const RegisterForm =(props) =>{
 
     const handleChange=() =>async(e)=>{
         let emailData= email.current.value;
-        let photoData=data.photo
+        let photoData
+        if (window.innerWidth<1024) {
+            photoData=photo.current.value
+        }else{
+            photoData=data.photo
+        }
         let passwordData = password.current.value;
 
         setData({...data,"email" : emailData,
@@ -46,9 +50,9 @@ const RegisterForm =(props) =>{
         <legend className="text-xs">Email</legend>
         <input  ref={email} className="w-full h-5 rounded-xl " onChange={handleChange()} type="email" />
     </fieldset>
-    {isMobile ?(<fieldset className="border-solid border-2 border-black rounded-xl">
+    {window.innerWidth<1024 ?(<fieldset className="border-solid border-2 border-black rounded-xl">
         <legend className="text-xs">Photo</legend>
-        <input className="w-full h-5 rounded-xl" type="url" placeholder="Insert URL" onChange={handleChange()} />
+        <input ref={photo} className="w-full h-5 rounded-xl" type="url" placeholder="Insert URL" onChange={handleChange()} />
     </fieldset>): (<div className="">
         <legend className="text-xs">Photo</legend>
         <input className="w-full h-fit" type="file" onChange={(e)=>setPhoto(e)} />
@@ -63,7 +67,9 @@ const RegisterForm =(props) =>{
             <input  type="checkbox" />Send notification to my email
         </label>
     </div>
-     <input onClick={(e)=>setFile(e)} className="h-8 bg-blue-700 rounded-xl mt-3 cursor-pointer" type="submit" value="SingUp"/>
+    {window.innerWidth<1024 ? (<input onClick={(e)=>handleCreate(e,data)} className="h-8 bg-blue-700 rounded-xl mt-3 cursor-pointer" type="submit" value="SingUp"/>
+):(     <input onClick={(e)=>setFile(e)} className="h-8 bg-blue-700 rounded-xl mt-3 cursor-pointer" type="submit" value="SingUp"/>
+)}
      <button  className="flex justify-center mt-3 gap-3 rounded-xl bg-slate-50 items-center h-8"><img src="../images/Google.png" alt="" /><p>Sign up with Google</p></button>
   <div className="text-center flex flex-col gap-3 mt-3 text-xs">
     <p>Already have an account? <Link className="text-blue-700" to={"/signIn"}>Sign In</Link> </p>
